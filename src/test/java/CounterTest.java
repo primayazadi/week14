c9dc45c7d1b286c180f97789480c6a4281933675
primayazadi <127044669+primayazadi@users.noreply.github.com>
2024-05-31 10:49:35 +0700

    contoh error

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -20,16 +20,6 @@ public class CounterTest {
 	}
 
 	@Test
-	public void testIncrement() {
-		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
-			testCounter.increment();
-			assertEquals(testCounter.getCount(), i);
-		}
-	}
-
-	@Test
 	public void testDecrement() {
 		Counter testCounter = new Counter();
 		
